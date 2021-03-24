916c1c7c8f26817bd91cdeba0d4da94f01b25130
alebarrientos <alexis.barrientos@mercadolibre.com>
2021-03-23 22:16:46 -0700

    radixSort Implemented

--- src/practica01/RadixSortEjerc.java
+++ src/practica01/RadixSortEjerc.java
@@ -1,10 +1,72 @@
 package practica01;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.SplittableRandom;
+
 public class RadixSortEjerc
 {
     public static void radixSort(int []arr)
     {
-        // PROGRAMAR AQUI
+        // Convierto el array(en int) a un array de strings
+        String sArr[] = StringUtil.toStringArray(arr);
+
+        //Normalizamos el array de string completando de 0
+        StringUtil.lNormalize(sArr, '0');
+
+        // creamos un hashmap de tipo arrayList de indice de integer y valor de tipo arrayList
+        HashMap<Integer, ArrayList<String>> hm = new HashMap<>();
+
+        // Inicio una variable con la cant digitos de la posicion 0 (longitud)
+        int digitos = sArr[0].length();
+
+        //
+        for (int i=0; i<digitos;i++){
+            // agrego 10 entradas de 0 a 9 con arrayList vacios
+            iniciarHm(hm);
+
+            // Inicio una variable con los digitos -1 - la posicion del for
+            int posDigito = digitos-1-i;
+            for (int j=0; j< sArr.length;j++){
+
+                // Donde asignamos c/ posicion como charAt con 0 para que no lo contemple
+                int d = sArr[j].charAt(posDigito)-'0';
+
+                // la posicion d le agregamos la posicion j del array de tipo string (sArr)
+                hm.get(d).add(sArr[j]);
+            }
+
+            //
+            rearmarArr(sArr,hm);
+        }
+        int iArr[] = StringUtil.toIntArray(sArr);
+        for (int x=0; x<iArr.length; x++){
+            arr[x] = iArr[x];
+        }
+
+    }
+
+    // Se rearma el metodo hashmap
+    private static void rearmarArr(String[] sArr, HashMap<Integer, ArrayList<String>> hm ) {
+        int k = 0;
+        for (int i=0; i<10 ; i++){
+
+            // le asigno c/ posicion del hashmap
+            ArrayList<String> x = hm.get(i);
+            for(int j=0; j < x.size();j++){
+
+                // asiganmos de c/ posicion del array de string la posicion de j
+                sArr[k++] = x.get(j);
+            }
+        }
+    }
+
+    private static void iniciarHm(HashMap<Integer, ArrayList<String>> hm ) {
+        hm.clear();
+        for (int i=0;i <10;i++){
+            hm.put(i, new ArrayList<>());
+
+        }
     }
 
     public static void main(String[] args)
@@ -14,8 +76,9 @@ public class RadixSortEjerc
 
         for(int i=0; i<arr.length;i++)
         {
-            System.out.print(arr[i]+(i<arr.length-1?",":""));
+            System.out.println(arr[i]+(i<arr.length-1?",":""));
         }
+
     }
 }
 
--- src/practica01/StringUtil.java
+++ src/practica01/StringUtil.java
@@ -6,7 +6,11 @@ public class StringUtil
     // Ejemplo: replicate('x',5) ==> 'xxxxx'
     public static String replicate(char c, int n)
     {
-        return "";
+        String transform = "";
+        for (int i=0; i < n; i++){
+            transform += c;
+        }
+        return transform;
     }
 
     // Retorna una cadena de longitud n, compuesta por s
@@ -15,33 +19,53 @@ public class StringUtil
     // Ejemplo lpad("5",3,'0') ==> "005"
     public static String lpad(String s, int n, char c)
     {
-        return "";
+        return replicate(c,n - s.length())+s;
     }
 
     // Retorna un String[] conteniendo los elementos de arr
     // representados como cadenas de caracteres
     public static String[] toStringArray(int arr[])
     {
-        return null;
+        String sArr[] = new String[arr.length];
+        for(int i=0; i< arr.length; i++){
+            sArr[i] = Integer.toString(arr[i]);
+        }
+        return sArr;
     }
 
-    // Retorna un String[] conteniendo los elementos de arr
+    // Retorna un int[] conteniendo los elementos de arr
     // representados como cadenas de caracteres
     public static int[] toIntArray(String arr[])
     {
-        return null;
+        int iArr[] = new int[arr.length];
+        for(int i=0; i< arr.length; i++){
+            iArr[i] = Integer.parseInt(arr[i]);
+        }
+        return iArr;
     }
 
     // Retorna la longitud del elemento con mayor cantidad
     // de caracteres del array arr
     public static int maxLength(String arr[])
     {
-        return 0;
+        int i = 0;
+        int max = arr[i].length();
+        for (String s : arr ) {
+            if (s.length() > max){
+                max = s.length();
+            }
+        }
+
+        return max;
     }
 
     // Completa los elemento del arr agregando caracteres c
     // a la izquierda, dejando a todos con la longitud del mayor
     public static void lNormalize(String arr[], char c)
     {
+        int max = maxLength(arr);
+        for(int i=0; i< arr.length;i++) {
+            arr[i] = lpad(arr[i],max, c);
+        }
     }
 }
